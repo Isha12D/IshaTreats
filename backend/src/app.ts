031e5c7cca1a596a237b1d6ac354ef14b70502c9
Isha12D <ishadoifode6@gmail.com>
2025-12-15 00:24:28 +0530

    added frontend deployed link

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,7 +6,13 @@ import orderRoutes from "./routes/order"
 
 const app = express();
 
-app.use(cors());
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+
+app.use(cors({
+  origin: [FRONTEND_URL],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.get("/", (_req, res) => {
